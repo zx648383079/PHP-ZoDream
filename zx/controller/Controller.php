0a9d453c0580909b5a0a35713e14eee628b92002
zx648383079 <648383079@qq.com>
2015-07-29 19:59:41 +0800

    g

--- zx/controller/Controller.php
+++ zx/controller/Controller.php
@@ -16,4 +16,27 @@
 		{
 			require(ZXV.$name.".php");
 		} 
+		
+		function ajaxJson($data,$type='JSON')
+		{
+			switch (strtoupper($type)){
+	            case 'JSON' :
+	                // 返回JSON数据格式到客户端 包含状态信息
+	                header('Content-Type:application/json; charset=utf-8');
+	                exit(json_encode($data));
+	            case 'XML'  :
+	                // 返回xml格式数据
+	                header('Content-Type:text/xml; charset=utf-8');
+	                exit(xml_encode($data));
+	            case 'JSONP':
+	                // 返回JSON数据格式到客户端 包含状态信息
+	                header('Content-Type:application/json; charset=utf-8');
+	                $handler  =   isset($_GET['callback']) ? $_GET['callback'] : 'jsonpReturn';
+	                exit($handler.'('.json_encode($data).');');  
+	            case 'EVAL' :
+	                // 返回可执行的js脚本
+	                header('Content-Type:text/html; charset=utf-8');
+	                exit($data);            
+	        }
+		}
 	}
