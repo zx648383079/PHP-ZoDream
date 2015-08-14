60f7557d14cbc59e5104300390e645fa4f804028
Jason ZOU <jason.zou@nwayschina.com>
2015-08-14 18:30:01 +0800

    整理

--- include/app.php
+++ include/app.php
@@ -7,6 +7,8 @@
 	
 	
 	
+	/************************ ↓定义常量↓ ************************/
+	
 	//获取域名或主机地址 
 	define('HTTP_HOST', $_SERVER['HTTP_HOST']); #localhost 
 	
@@ -18,46 +20,37 @@
 	define("NWAYSCONTROLLER","include/controller/");                   //定义控制器目录
 	define("NWAYSCF",".class.php");                       	//定义类文件后缀
 	
+	/************************* ↑定义常量↑ ***********************************/
+	
+	
+	/************************ ↓包含文件↓ ************************/
+	
 	include_once(NWAYS."function.php");                  //加载全局方法文件
 	
 	include_once(NWAYSCLASS."lang".NWAYSCF);
 	
-	new Lang(getLang());                                 //加载语言包 
-	
 	require(NWAYSCLASS."pdo".NWAYSCF);                    //加载数据库类文件
 	
-	if(!file_exists(NWAYSCONF."config.php"))
+	include_once(NWAYSCLASS."lang".NWAYSCF);                    //加载语言库类文件
+	
+	include_once(NWAYSCLASS."auth".NWAYSCF);                     //加载用户类
+	
+	include_once(NWAYSCONTROLLER."Controller.php");               //加载控制器基类
+	
+	/************************* ↑包含文件↑ ***********************************/
+	
+	/************************ ↓执行文件中的方法↓ ************************/
+	
+	new Lang(getLang());                                 //加载语言包 
+	
+	if(!is_file(NWAYSCONF."config.php"))
 	{
 		redirect('/install.php');
 	}
 	
-	
 	session_start();                                  //打开session 因为所有的地方都会先判断session
-	
-	include_once(NWAYSCLASS."auth".NWAYSCF);                     //加载用户类
-	
-	
-	include_once(NWAYSCONTROLLER."Controller.php");               //加载控制器基类
-	
 	getCAV();
 	
-	//$_SERVER["REQUEST_URI"];    //获取网址
-	//加载控制器
-	/*$con = isset($_GET['c'])?ucfirst($_GET['c']):'Home';
-	$name=$con."Controller";
-	$view=isset($_GET['v'])?$_GET['v']:'index';
+	/************************* ↑执行文件中的方法↑ ***********************/
 	
-	$controllerfile=NWAYSCONTROLLER.$name.".php";
 	
-	if(is_file($controllerfile))
-	{
-		require_once($controllerfile);
-		if( class_exists($name))
-		{
-			$controller=new $name;
-			if(method_exists($controller,$view))
-			{
-				$controller->$view();
-			}
-		}
-	}*/
