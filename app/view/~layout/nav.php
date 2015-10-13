7201890cb1fd703e907a0ff0534497486b134219
Jason Zou <jason.zou@nwayschina.com>
2015-10-13 17:30:07 +0800

    修改

--- app/view/~layout/nav.php
+++ app/view/~layout/nav.php
@@ -8,8 +8,6 @@ use App\Lib\Auth;
    </div>
   <ul>
     <li class="active"><a href="<?php App::url('/'); ?>">主页</a></li>
-    <li><a href="<?php App::url('?v=method'); ?>">方法</a></li>
-    <li><a href="<?php App::url('?v=create'); ?>">提交</a></li>
    <?php if(App::role('2')){?>
     <li><a href="<?php App::url('?c=admin'); ?>">后台</a></li>
    <?php } ?>
