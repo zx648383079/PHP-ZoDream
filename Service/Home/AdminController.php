aaecf36e3ff3d8a2fffdc0f245b5b462b394c7c2
zx648383079 <648383079@qq.com>
2016-05-12 17:10:37 +0800

    增加

--- Service/Home/AdminController.php
+++ Service/Home/AdminController.php
@@ -1,8 +1,10 @@
 <?php
 namespace Service\Home;
 
-
+use Zodream\Domain\Filter\Filters\PhoneFilter;
+use Zodream\Domain\SMS\Ihuyi;
 use Zodream\Infrastructure\Log;
+use Zodream\Infrastructure\ObjectExpand\StringExpand;
 use Zodream\Infrastructure\Request;
 
 class AdminController extends Controller {
@@ -17,5 +19,27 @@ class AdminController extends Controller {
     function indexPost($post) {
         Log::save($post->get(), 'honeypot');
         $this->send('message', '账户不存在或密码错误！');
+    }
+    
+    function smsAction($mobile) {
+        if (empty($mobile) || !(new PhoneFilter())->validate($mobile)) {
+            $this->ajaxReturn(array(
+                'status' => 'failure',
+                'error' => '手机号不正确！'
+            ));
+        }
+        $code = StringExpand::randomNumber(6);
+        $sms = new Ihuyi();
+        $result = $sms->send($mobile, $code);
+        if ($result === false) {
+            $this->ajaxReturn(array(
+                'status' => 'failure',
+                'error' => $sms->getError()
+            ));
+        }
+        $this->ajaxReturn(array(
+            'status' => 'success',
+            'code' => $code
+        ));
     }
 }
--- html/test.php
+++ html/test.php
@@ -1,6 +1,1 @@
 <?php
-define('APP_DIR', dirname(dirname(__FILE__)));            //定义路径
-define('APP_MODULE', 'Home');                            //定义组件名
-require_once(APP_DIR.'/vendor/autoload.php');
-\Zodream\Infrastructure\Session::getInstance();
-var_dump($_SESSION);
