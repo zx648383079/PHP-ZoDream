a1024e4d36762492fabd526837e5051e21b38e29
zx648383079 <648383079@qq.com>
2016-06-03 16:27:22 +0800

    update

---
+++ UserInterface/Admin/Waste/index.php
@@ -0,0 +1,59 @@
+<?php
+defined('APP_DIR') or exit();
+$this->extend(array(
+        'layout' => array(
+            'head'
+        ))
+);
+$page = $this->get('page');
+?>
+<div>
+    
+    <form method="POST">
+        随想：<textarea rows="1" cols="40" placeholder="内容" name="content" required></textarea>
+        <button type="submit">发表</button>
+    </form>
+</div>
+
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>内容</th>
+        <th>操作</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($page->getPage() as $value) {?>
+            <tr>
+                <form method="POST">
+                    <td><?php echo $value['id'];?></td>
+                    <td>
+                        <input type="hidden" name="id" value="<?php echo $value['id'];?>">
+                        <textarea rows="1" cols="40"  name="content"><?php echo $value['content'];?></textarea>
+                    </td>
+                    <td>
+                        <button type="submit">修改</button>
+                        
+                        <a href="#" class="btn">删除</a>
+                    </td>
+                </form>
+            </tr>
+        <?php }?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="3">
+                <?php $page->pageLink();?>
+            </th>
+        </tr>
+    </tfoot>
+</table>
+
+<?php
+$this->extend(array(
+        'layout' => array(
+            'foot'
+        ))
+);
+?>
---
+++ UserInterface/Home/Waste/index.php
@@ -0,0 +1,68 @@
+<?php
+defined('APP_DIR') or exit();
+$this->extend(array(
+	'layout' => array(
+		'head',
+        'navbar'
+	)), array(
+        'zodream/blog.css'
+    )
+);
+$page = $this->get('page');
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            <ul class="term">
+                <li><a href="<?php $this->url('blog');?>">全部</a></li>
+                <?php foreach ($this->get('term', array()) as $item) {?>
+                    <li><a href="<?php $this->url(null, array('termid' => $item['id']));?>"><?php echo $item['name'];?></a></li>
+                <?php }?>
+            </ul>
+        </div>
+        <div id="list" class="col-md-10">
+            <ul class="list">
+                <?php foreach ($page->getPage() as $item) {?>
+                    <li class="list-item">
+                        <div class="recommend" data="<?php echo $item['id'];?>">
+                            <span><?php echo $item['recommend']?></span>
+                            <span>推荐</span>
+                        </div>
+                        <h4 class="list-item-head">
+                            <a href="<?php $this->url('blog/view/id/'.$item['id']);?>">
+                                <?php echo $item['title'];?>
+                            </a>
+                        </h4>
+                        <div class="list-item-content">
+                            <?php echo $item['excerpt'];?>
+                        </div>
+                        <div class="list-item-foot">
+                            <a href="<?php $this->url('blog/user/'.$item['user_id']);?>"><?php echo $item['user'];?></a>
+                             发表于 
+                            <?php $this->ago($item['create_at']);?>   
+                            分类：<?php echo $item['term'];?>
+                             评论（<?php echo $item['comment_count'];?>）
+                        </div>
+                    </li>
+                <?php }?>
+            </ul>
+            <div>
+                <?php $page->pageLink();?>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php
+$this->extend(array(
+	'layout' => array(
+		'foot'
+	)), array(
+        function() {?>
+<script type="text/javascript">
+require(['home/blog']);
+</script>       
+     <?php }
+    )
+);
+?>
