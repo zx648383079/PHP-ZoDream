1c65f821df19afa8d2e8ea071264977305de709d
zx648383079 <648383079@qq.com>
2016-09-07 10:19:35 +0800

    修改

--- html/test.php
+++ html/test.php
@@ -2,10 +2,4 @@
 define('DEBUG', true);                  //是否开启测试模式
 define('APP_DIR', dirname(dirname(__FILE__)));            //定义路径
 require_once(APP_DIR.'/vendor/autoload.php');
-$i = 0;
-preg_replace_callback('/\d/', function ($matches) use (&$i) {
-    $i ++;
-    var_dump($i);
-    var_dump($matches);
-    return '';
-}, '1234567');
+var_dump();
