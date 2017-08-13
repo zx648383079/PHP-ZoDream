3cf982681875a16408338a5a8f197b017b89ae3e
ZoDream <648383079@qq.com>
2017-08-13 22:08:51 +0800

    修改

--- Module/WeChat/Module.php
+++ Module/WeChat/Module.php
@@ -1,8 +1,20 @@
 <?php
 namespace Module\WeChat;
 
+use Module\WeChat\Domain\Migrations\CreateWeChatTables;
 use Zodream\Service\Controller\Module as BaseModule;
 
 class Module extends BaseModule {
 
+    public function insert() {
+        $this->getMigration()->up();
+    }
+
+    public function uninstall() {
+        $this->getMigration()->up();
+    }
+
+    public function getMigration() {
+        return new CreateWeChatTables();
+    }
 }
