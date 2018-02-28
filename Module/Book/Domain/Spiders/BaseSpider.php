df67666c27aefe27bed64fb802dece4e266dc4ab
zx648383079 <648383079@qq.com>
2018-02-28 14:32:45 +0800

    修改

---
+++ Module/Book/Domain/Spiders/BaseSpider.php
@@ -0,0 +1,40 @@
+<?php
+namespace Module\Book\Domain\Spiders;
+
+use Module\Book\Domain\Model\BookChapterModel;
+use Module\Book\Domain\Model\BookModel;
+use Zodream\Spider\Support\Html;
+use Zodream\Spider\Support\Uri;
+
+abstract class BaseSpider {
+
+    /**
+     * @param Uri $uri
+     * @return boolean
+     */
+    abstract public function isCatalogPage(Uri $uri);
+
+    /**
+     * @param Uri $uri
+     * @return boolean
+     */
+    abstract public function isContentPage(Uri $uri);
+
+    /**
+     * @param $html
+     * @return BookModel
+     */
+    abstract public function getBook(Html $html);
+
+    /**
+     * @param $html
+     * @return BookChapterModel[]
+     */
+    abstract public function getCatalog(Html $html);
+
+    /**
+     * @param $html
+     * @return string
+     */
+    abstract public function getContent(Html $html);
+}
