7037cc40f500a5398b0a1f5ca4ec55a6696a8bc7
zodream <648383079@qq.com>
2018-05-04 18:58:25 +0800

    增加滑动验证码

--- Service/Account/CaptchaController.php
+++ Service/Account/CaptchaController.php
@@ -4,6 +4,7 @@ namespace Service\Account;
  * 验证码
  */
 use Zodream\Image\Captcha;
+use Zodream\Image\SlideCaptcha;
 use Zodream\Infrastructure\Http\Request;
 use Zodream\Service\Factory;
 
@@ -27,4 +28,57 @@ class CaptchaController extends Controller {
         ]);
 		return Factory::response()->image($captcha->generate($level));
 	}
+
+	function slideAction() {
+        $img = new SlideCaptcha(Factory::public_path()->file('assets/images/banner.jpg'));
+        $img->scale(400, 250);
+        $img->setShape('E:\Desktop\1.jpg');
+        $img->generate();
+
+        $args = range(0, 7);
+        shuffle($args);
+        list($bg, $points, $size) = $img->sortBy($args);
+        $html = '';
+        foreach ($points as $point) {
+            $html .= sprintf('<div class="slide-img" style="background-position: %spx %spx"></div>', $point[0], $point[1]);
+        }
+        $width = $img->getWidth();
+        $height = $img->getHeight();
+        $bg_data = $bg->toBase64();
+        $html = <<<HTML
+<style>
+.slide-box {
+    width: {$width}px;
+    height: {$height}px;
+    position: relative;
+}
+.slide-box .slide-img {
+    float: left;
+    margin: 0;
+    padding: 0;
+    background-image: url({$bg_data});
+    background-repeat: no-repeat;
+    width: {$size[0]}px;
+    height: {$size[1]}px;
+}
+.slide-box .slide-cut {
+    position: absolute;
+    top: {$img->getPoint()[1]}px;
+    background-image: url({$img->getSlideImage()->toBase64()});
+    background-repeat: no-repeat;
+    width: {$img->getSlideImage()->getWidth()}px;
+    height: {$img->getSlideImage()->getHeight()}px;
+    z-index: 9;
+}
+</style>
+<div class="slide-box">
+    <div class="slide-cut"></div>
+    <div class="slide-list">
+        {$html}
+    </div>
+    
+</div>
+HTML;
+        return $this->showContent($html);
+    }
 }
