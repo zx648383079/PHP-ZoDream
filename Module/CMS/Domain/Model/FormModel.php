bdecea3c77dc25f6a35ac69950d90425c6d42d22
ZoDream <648383079@qq.com>
2018-05-25 23:22:02 +0800

    修改

---
+++ Module/CMS/Domain/Model/FormModel.php
@@ -0,0 +1,9 @@
+<?php
+namespace Module\CMS\Domain\Model;
+
+class FormModel extends BaseModel {
+
+    public static function tableName() {
+        return 'cms_form';
+    }
+}
--- Module/CMS/Service/Admin/CategoryController.php
+++ Module/CMS/Service/Admin/CategoryController.php
@@ -3,7 +3,32 @@ namespace Module\CMS\Service\Admin;
 
 class CategoryController extends Controller {
     public function indexAction() {
-        $
+
         return $this->show();
+    }
+
+    public function createAction() {
+        return $this->runMethod('edit', ['id' => null]);
+    }
+
+    public function editAction($id) {
+        $model = ProjectModel::findOrNew($id);
+        $project_list = ProjectModel::select('name', 'id')->all();
+        return $this->show(compact('model', 'project_list'));
+    }
+
+    public function saveAction() {
+        $model = new ProjectModel();
+        if (!$model->load() || !$model->autoIsNew()->setMock()->save()) {
+            return $this->jsonFailure($model->getFirstError());
+        }
+        return $this->jsonFailure($model->getFirstError());
+    }
+
+    public function deleteAction($id) {
+        ProjectModel::where('id', $id)->delete();
+        return $this->jsonSuccess([
+            'url' => $this->getUrl('')
+        ]);
     }
 }
