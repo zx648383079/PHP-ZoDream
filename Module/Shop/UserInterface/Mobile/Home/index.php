7a7872e7cd9c7e6b1ba65f03e3b3e07b61b115a7
ZoDream <648383079@qq.com>
2018-06-08 23:32:20 +0800

    修改

--- Module/Shop/UserInterface/Mobile/Home/index.php
+++ Module/Shop/UserInterface/Mobile/Home/index.php
@@ -6,6 +6,35 @@ $this->title = 'ZoDream';
 $this->extend('../layouts/search');
 ?>
 
+<div class="banner-box">
 
+</div>
+
+<div class="goods-list">
+    <div class="item-view">
+        <div class="item-img">
+            <img src="/assets/images/goods.webp" alt="">
+        </div>
+        <div class="item-title">
+            1233333
+        </div>
+        <div class="item-actions">
+            <span class="item-price">555</span>
+            <span>加入购物车</span>
+        </div>
+    </div>
+    <div class="item-view">
+        <div class="item-img">
+            <img src="/assets/images/goods.webp" alt="">
+        </div>
+        <div class="item-title">
+            1233333
+        </div>
+        <div class="item-actions">
+            <span class="item-price">555</span>
+            <span>加入购物车</span>
+        </div>
+    </div>
+</div>
 
 <?php $this->extend('../layouts/navbar');?>
