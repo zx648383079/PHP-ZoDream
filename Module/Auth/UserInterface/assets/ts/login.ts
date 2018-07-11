86099a118e335f2b719014f0cdc5047ae00efdc7
zodream <648383079@qq.com>
2018-07-11 19:22:31 +0800

    修改

--- Module/Auth/UserInterface/assets/ts/login.ts
+++ Module/Auth/UserInterface/assets/ts/login.ts
@@ -9,8 +9,9 @@ $(function() {
         $(this).parents('.input-group').removeClass('input-focus');
     });
     $(".login-form").submit(function() {
-        let $this = $(this);
-        postJson($this.attr('action'), $this.serialize(), function(data) {
+        let _this = $(this),
+            loading = Dialog.loading();
+        postJson(_this.attr('action'), _this.serialize(), function(data) {
             parseAjax(data);
         });
         return false;
