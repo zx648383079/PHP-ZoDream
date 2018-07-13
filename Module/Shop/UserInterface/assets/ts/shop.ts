8f7e9309832281900fd466fe5e3454d88dbf1fbe
zodream <648383079@qq.com>
2018-07-13 10:55:20 +0800

    修改

--- Module/Shop/UserInterface/assets/ts/shop.ts
+++ Module/Shop/UserInterface/assets/ts/shop.ts
@@ -16,3 +16,46 @@ function collectGoods(id: number, target?: any) {
         target && $(target).toggleClass('active', data.data);
     });
 }
+
+function mapItem(cb: (JQuery) => any) {
+    $(".goods-item").each(function () {
+        if (cb($(this)) == false) {
+            return false;
+        }
+    });
+}
+
+function mapCheckedItem(cb: (JQuery) => any) {
+    mapItem(function (goods: JQuery) {
+        if (!goods.find('.check-box').hasClass('active')) {
+            return;
+        }
+        if (cb(goods) == false) {
+            return false;
+        }
+    });
+}
+
+$(function() {
+    $(".check-box").click(function() {
+        $(this).toggleClass('active').trigger('change');
+    });
+    $('.number-box .fa-minus').click(function() {
+        let input = $(this).closest('.number-box').find('input'),
+            min = input.attr('min') || 0,
+            amount = Number(input.val()) || 0;
+        if (amount > min) {
+            amount -= 1;
+        }
+        input.val(amount).trigger('change');
+    });
+    $('.number-box .fa-plus').click(function() {
+        let input = $(this).closest('.number-box').find('input'),
+            max = input.attr('max') || 1,
+            amount = Number(input.val()) || 0;
+        if (amount < max) {
+            amount += 1;
+        }
+        input.val(amount).trigger('change');
+    });
+});
