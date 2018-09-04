8768ae0073e54b897bf1216d1568a4d780076ac7
zodream <648383079@qq.com>
2018-09-04 18:36:57 +0800

    修改

--- Module/Shop/UserInterface/Admin/layouts/main.php
+++ Module/Shop/UserInterface/Admin/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 defined('APP_DIR') or exit();
 use Zodream\Template\View;
+use Zodream\Html\Dark\Layout;
 
 /** @var $this View */
 $this->registerCssFile([
@@ -11,83 +12,154 @@ $this->registerCssFile([
         '@shop_admin.css'
     ])->registerJsFile([
         '@jquery.min.js',
+        '@jquery.pjax.min.js',
         '@jquery.dialog.min.js',
         '@jquery.upload.min.js',
         '@main.min.js',
         '@shop_admin.min.js'
     ]);
 ?>
-<!DOCTYPE html>
-<html lang="<?=$this->get('language', 'zh-CN')?>">
-   <head>
-       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-       <meta name="Description" content="<?=$this->description?>" />
-       <meta name="keywords" content="<?=$this->keywords?>" />
-       <title><?=$this->title?></title>
-       <?=$this->header();?>
-   </head>
-   <body>
-   <header>
-        <div class="container">
-            ZoDream Shop Admin
-        </div>
-    </header>
-    <div class="container page-box">
-        <div class="left-catelog navbar">
-            <span class="left-catelog-toggle"></span>
-            <ul>
-                <li><a href="<?=$this->url('./admin')?>"><i class="fa fa-home"></i><span>首页</span></a></li>
-                <li class="expand"><a href="javascript:;"><i class="fa fa-briefcase"></i><span>商品管理</span></a>
-                    <ul>
-                        <li><a href="<?=$this->url('./admin/goods')?>"><i class="fa fa-list"></i><span>商品列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/goods/create')?>"><i class="fa fa-list"></i><span>新建商品</span></a></li>
-                        <li><a href="<?=$this->url('./admin/category')?>"><i class="fa fa-list"></i><span>分类列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/category/create')?>"><i class="fa fa-list"></i><span>新建分类</span></a></li>
-                        <li><a href="<?=$this->url('./admin/brand')?>"><i class="fa fa-list"></i><span>品牌列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/brand/create')?>"><i class="fa fa-list"></i><span>新建品牌</span></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;"><i class="fa fa-briefcase"></i><span>订单管理</span></a>
-                    <ul>
-                        <li><a href="<?=$this->url('./admin/order')?>"><i class="fa fa-list"></i><span>订单列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/order/create')?>"><i class="fa fa-list"></i><span>新建订单</span></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;"><i class="fa fa-briefcase"></i><span>文章管理</span></a>
-                    <ul>
-                        <li><a href="<?=$this->url('./admin/article')?>"><i class="fa fa-list"></i><span>文章列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/article/create')?>"><i class="fa fa-list"></i><span>新建文章</span></a></li>
-                        <li><a href="<?=$this->url('./admin/article/category')?>"><i class="fa fa-list"></i><span>分类列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/article/create_category')?>"><i class="fa fa-list"></i><span>新建分类</span></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;"><i class="fa fa-briefcase"></i><span>广告管理</span></a>
-                    <ul>
-                        <li><a href="<?=$this->url('./admin/ad')?>"><i class="fa fa-list"></i><span>广告列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/ad/create')?>"><i class="fa fa-list"></i><span>新建广告</span></a></li>
-                        <li><a href="<?=$this->url('./admin/ad/position')?>"><i class="fa fa-list"></i><span>广告位列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/ad/create_position')?>"><i class="fa fa-list"></i><span>新建广告位</span></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;"><i class="fa fa-briefcase"></i><span>商城设置</span></a>
-                    <ul>
-                        <li><a href="<?=$this->url('./admin/payment')?>"><i class="fa fa-list"></i><span>支付列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/payment/create')?>"><i class="fa fa-list"></i><span>新建支付</span></a></li>
-                        <li><a href="<?=$this->url('./admin/shipping')?>"><i class="fa fa-list"></i><span>配送列表</span></a></li>
-                        <li><a href="<?=$this->url('./admin/shipping/create')?>"><i class="fa fa-list"></i><span>新建配送</span></a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <div class="right-content">
-            <?=$content?>
-        </div>
-    </div>
-   <?=$this->footer()?>
-   </body>
-</html>
+
+<?= Layout::mainIfPjax($this, [
+    [
+        '首页',
+        './admin',
+        'fa fa-home',
+    ],
+    [
+        '商品管理',
+        null,
+        'fa-briefcase',
+        [
+            [
+                '商品列表',
+                './admin/goods',
+                'fa fa-list'
+            ],
+            [
+                '新建商品',
+                './admin/goods/create',
+                'fa fa-plus'
+            ],
+            [
+                '分类列表',
+                './admin/category',
+                'fa fa-list'
+            ],
+            [
+                '新建分类',
+                './admin/category/create',
+                'fa fa-plus'
+            ],
+            [
+                '品牌列表',
+                './admin/brand',
+                'fa fa-list'
+            ],
+            [
+                '新建品牌',
+                './admin/brand/create',
+                'fa fa-plus'
+            ],
+        ],
+        true
+    ],
+    [
+        '订单管理',
+        null,
+        'fa-briefcase',
+        [
+            [
+                '订单列表',
+                './admin/order',
+                'fa fa-list'
+            ],
+            [
+                '新建订单',
+                './admin/order/create',
+                'fa fa-plus'
+            ],
+        ],
+    ],
+    [
+        '文章管理',
+        null,
+        'fa-book',
+        [
+            [
+                '文章列表',
+                './admin/article',
+                'fa fa-list'
+            ],
+            [
+                '新建文章',
+                './admin/article/create',
+                'fa fa-plus'
+            ],
+            [
+                '分类列表',
+                './admin/article/category',
+                'fa fa-list'
+            ],
+            [
+                '新建分类',
+                './admin/article/create_category',
+                'fa fa-plus'
+            ],
+        ],
+    ],
+    [
+        '广告管理',
+        null,
+        'fa-book',
+        [
+            [
+                '广告列表',
+                './admin/ad',
+                'fa fa-list'
+            ],
+            [
+                '新建广告',
+                './admin/ad/create',
+                'fa fa-plus'
+            ],
+            [
+                '广告位列表',
+                './admin/ad/position',
+                'fa fa-list'
+            ],
+            [
+                '新建广告位',
+                './admin/ad/create_position',
+                'fa fa-plus'
+            ],
+        ],
+    ],
+    [
+        '商城设置',
+        null,
+        'fa fa-briefcase',
+        [
+            [
+                '支付列表',
+                './admin/payment',
+                'fa fa-list'
+            ],
+            [
+                '新建支付',
+                './admin/payment/create',
+                'fa fa-plus'
+            ],
+            [
+                '配送列表',
+                './admin/shipping',
+                'fa fa-list'
+            ],
+            [
+                '新建配送',
+                './admin/shipping/create',
+                'fa fa-plus'
+            ],
+        ],
+    ]
+], 'ZoDream Shop Admin') ?>
