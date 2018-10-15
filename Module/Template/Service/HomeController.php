bd631845d8b08c18ed265012be22d65fe475ce7f
zodream <648383079@qq.com>
2018-10-15 18:39:06 +0800

    修改

--- Module/Template/Service/HomeController.php
+++ Module/Template/Service/HomeController.php
@@ -4,11 +4,15 @@ namespace Module\Template\Service;
 use Module\ModuleController;
 use Module\Template\Domain\Model\PageModel;
 use Module\Template\Domain\Page;
+use Exception;
 
 class HomeController extends ModuleController {
 
     public function indexAction($site = 0, $id = 0) {
         $model = PageModel::findByIdOrSite($id, $site);
+        if (empty($model)) {
+            throw new Exception('Site NOT FOUND!');
+        }
         $page = new Page($model, false);
         return $page->render();
     }
