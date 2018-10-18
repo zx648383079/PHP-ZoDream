0cbf57e091f7bed5e7c58e9171d3b27b8b0da636
ZoDream <648383079@qq.com>
2018-10-18 22:44:33 +0800

    修改

--- UserInterface/Home/Home/index.php
+++ UserInterface/Home/Home/index.php
@@ -6,7 +6,7 @@ $this->extend('layouts/header');
 ?>
 
 <div class="metro-grid">
-    <div class="item hover-middle">
+    <div class="item hover-left">
         <a href="<?=$this->url('blog')?>">
             <div class="font-gird">
                 <img src="/assets/images/blog.png" alt="">
@@ -17,6 +17,20 @@ $this->extend('layouts/header');
                 <div class="item-content">
                     本博客只为记录学习及工作。<br/>
                     具体功能正在开发中。。。
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="item hover-right">
+        <a href="<?=$this->url('tool')?>">
+            <div class="font-gird">
+                <img src="/assets/images/tool.png" alt="">
+                <p>工具</p>
+            </div>
+            <div class="back-grid">
+                <h3>简介</h3>
+                <div class="item-content">
+                    只为方便服务。。。
                 </div>
             </div>
         </a>
