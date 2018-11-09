61d48188685c5375ea45a80b20bdc9b03d8ffed7
zodream <648383079@qq.com>
2018-11-09 19:00:57 +0800

    修改

--- Module/Schedule/README.md
+++ Module/Schedule/README.md
@@ -8,12 +8,30 @@
 
 再加入系统的计划任务队列中
 
+
+Linux 
 ```shell
 php artisan schedule /dev/null 2>&1
 ```
+
+Windows 请手动添加计划任务
 
 ### 这里是指注册定时任务，任务执行时间为每分钟
 
 ### 注意
 
 本模块必须依赖操作系统的定时任务，本程序自身不具备自动执行功能
+
+## 添加任务
+
+在 Service/config/schedule.php
+
+添加执行注册任务的类或方法
+
+```PHP
+
+function (Scheduler $scheduler) {
+    $scheduler->php('echo 1;');
+}
+
+```
--- Module/Schedule/Service/HomeController.php
+++ Module/Schedule/Service/HomeController.php
@@ -7,14 +7,19 @@ use Zodream\Service\Factory;
 
 class HomeController extends Controller {
     public function indexAction() {
-        $scheduler = new Scheduler();
+        $scheduler = new Scheduler(config('schedule', []));
         $this->registerSchedule($scheduler);
         $scheduler->run();
+        return $this->showContent('complete!');
     }
 
     protected function registerSchedule(Scheduler $scheduler) {
         $data = Factory::config()->getConfigByFile('schedule');
         foreach ($data as $item) {
+            if (is_callable($item)) {
+                call_user_func($item, $scheduler);
+                continue;
+            }
             if (!class_exists($item)) {
                 continue;
             }
