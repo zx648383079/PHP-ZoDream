bf75c4a7a754ef3170977980c1ec8f5723429b85
zodream <648383079@qq.com>
2018-12-06 18:54:27 +0800

    修改

--- Module/Shop/UserInterface/Mobile/layouts/search_back.php
+++ Module/Shop/UserInterface/Mobile/layouts/search_back.php
@@ -3,9 +3,9 @@
         <a class="home-btn" href="<?=$this->url('./mobile')?>">
             <i class="fa fa-home"></i>
         </a>
-        <form action="<?=$this->url('./mobile/search')?>">
+        <form onclick="window.location.href='<?=$this->url('./mobile/search')?>';">
             <i class="fa fa-search" aria-hidden="true"></i>
-            <input type="text" name="keywords" value="<?=$this->keywords?>" placeholder="搜索">
+            <input type="text" readonly name="keywords" value="<?=$this->keywords?>">
             <i class="fa fa-times-circle"></i>
         </form>
     </div>
--- Module/Shop/UserInterface/Mobile/layouts/search_empty.php
+++ Module/Shop/UserInterface/Mobile/layouts/search_empty.php
@@ -2,8 +2,8 @@
     <div class="search-box">
         <form action="<?=$this->url('./mobile/search')?>">
             <i class="fa fa-search" aria-hidden="true"></i>
-            <input type="text" name="keywords" value="<?=$this->keywords?>" placeholder="搜索">
-            <i class="fa fa-times-circle"></i>
+            <input type="text" name="keywords" value="<?=$this->keywords?>" placeholder="搜索" autocomplete="off" onkeyup="$(this).next().toggle(!!$(this).val())">
+            <i class="fa fa-times-circle hide" onclick="$(this).hide().prev().val('')"></i>
         </form>
         <a class="cancel-btn" href="javascript:history.back();">取消</a>
     </div>
