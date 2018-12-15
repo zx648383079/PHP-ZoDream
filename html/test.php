ffb29e6dd9cc5de4204118d8dffdbf65ba26aad1
zodream <648383079@qq.com>
2018-12-15 18:52:07 +0800

    修改

--- html/test.php
+++ html/test.php
@@ -12,3 +12,28 @@ use Module\Document\Domain\Model\ProjectModel;
 use Zodream\Http\Http;
 use Zodream\Helpers\Str;
 use Module\Shop\Domain\Model\RegionModel;
+use Zodream\Image\Canvas;
+use Zodream\Image\Image;
+use Zodream\Disk\File;
+use Zodream\Image\Node\Box;
+use Zodream\Image\Node\Point;
+use Zodream\Image\Node\Text;
+
+$goods = new File('D:\Documents\Github\PHP-ZoDream\html\assets\images\zd.jpg');
+$qr = new File('D:\Documents\Github\PHP-ZoDream\html\assets\images\wx.jpg');
+
+$font = 'D:\Documents\Github\PHP-ZoDream\data\fonts\msyh.ttc';
+
+$img = new Canvas();
+$img->create(402, 712);
+$img->setBackground('#fff')
+    ->addImage(new Image($goods), new Box(0, 60, 402, 402))
+    ->addImage(new Image($qr), new Box(18, 590, 106, 106))
+    ->addText(new Text('请长按识别二维码', 18, 560, [155, 143, 128], $font, 12))
+    ->addText(new Text('微信支付购买', 18, 576, [155, 143, 128], $font, 12))
+    ->addText(new Text('kiwigo', 300, 560, '#000', $font, 16))
+    ->addText(new Text('￥', 278, 640, '#f00', $font, 12))
+    ->addText(new Text('123.00', 290, 640, '#f00', $font, 25))
+    ->addText(new Text('测试商品', 300, 670, '#000', $font, 12))
+;
+app('response')->image($img)->send();
