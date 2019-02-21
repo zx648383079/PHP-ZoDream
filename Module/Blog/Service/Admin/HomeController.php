b3a1861d0446f945b7d40b980addb29629354245
zodream <648383079@qq.com>
2019-02-21 16:08:25 +0800

    修改

--- Module/Blog/Service/Admin/HomeController.php
+++ Module/Blog/Service/Admin/HomeController.php
@@ -1,9 +1,19 @@
 <?php
 namespace Module\Blog\Service\Admin;
 
+use Module\Blog\Domain\Model\BlogModel;
+use Module\Blog\Domain\Model\CommentModel;
+use Module\Blog\Domain\Model\TermModel;
+
 class HomeController extends Controller {
 
     public function indexAction() {
-        return $this->show();
+        $term_count = TermModel::query()->count();
+        $blog_id = BlogModel::where('user_id', auth()->id())->pluck('id');
+        $blog_count = count($blog_id);
+        $view_count = BlogModel::where('user_id', auth()->id())->sum('click_count');
+        $comment_count = CommentModel::whereIn('blog_id', $blog_id)
+            ->count();
+        return $this->show(compact('term_count', 'blog_count', 'view_count', 'comment_count'));
     }
 }
