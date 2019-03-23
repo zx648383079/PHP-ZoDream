57d9d1c28a44441f0a4d574c2150de95227faa26
zodream <648383079@qq.com>
2019-03-23 17:14:40 +0800

    修改

---
+++ Module/Shop/Service/Api/PayController.php
@@ -0,0 +1,34 @@
+<?php
+namespace Module\Shop\Service\Api;
+
+use Module\Shop\Domain\Model\OrderModel;
+use Module\Shop\Domain\Model\PayLogModel;
+
+class PayController extends Controller {
+
+    protected function methods() {
+        return ['index' => ['POST']];
+    }
+
+    public function indexAction($order, $payment) {
+        $order = OrderModel::find($order);
+        if ($order->status != OrderModel::STATUS_UN_PAY) {
+            return $this->renderFailure('此订单不能支付');
+        }
+        $log = PayLogModel::create([
+            'type' => PayLogModel::TYPE_ORDER,
+            'payment_id' => $payment,
+            'user_id' => auth()->id(),
+            'data' => $order->id,
+            'status' => PayLogModel::STATUS_NONE,
+            'amount' => $order->order_amount,
+        ]);
+        $data = [
+            'success' => 'true', // 直接支付成功，比如余额支付
+            'url' => '', // 返回链接需要跳转
+            'html' => '', // 返回html 需要输入会自动提交表单post
+            'params' => [] // 返回签名的参数，直接提交即可
+        ];
+        return $this->render(compact('data'));
+    }
+}
