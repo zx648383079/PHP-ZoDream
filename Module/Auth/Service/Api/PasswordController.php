2ced7919397e0895158ba579aa4f3b2f62fd2a97
ZoDream <648383079@qq.com>
2019-04-12 23:49:13 +0800

    修改

--- Module/Auth/Service/Api/PasswordController.php
+++ Module/Auth/Service/Api/PasswordController.php
@@ -1,9 +1,16 @@
 <?php
 namespace Module\Auth\Service\Api;
 
+use Module\Auth\Domain\Model\UserModel;
 use Zodream\Route\Controller\RestController;
 
 class PasswordController extends RestController {
+
+    protected function rules() {
+        return [
+            'update' => '@',
+        ];
+    }
 
     public function indexAction() {
         return $this->render([
@@ -13,5 +20,21 @@ class PasswordController extends RestController {
     public function resetAction() {
         return $this->render([
         ]);
+    }
+
+    public function updateAction($old_password, $password) {
+        /** @var UserModel $model */
+        $model = auth()->user();
+        if (strlen($password) < 6) {
+            return $this->renderFailure('密码必须大于6位！');
+        }
+        if (!$model->validatePassword($old_password)) {
+            return $this->renderFailure('密码不正确！');
+        }
+        $model->setPassword($password);
+        if ($model->save()) {
+            return $this->render(['data' => true]);
+        }
+        return $this->renderFailure($model->getFirstError());
     }
 }
