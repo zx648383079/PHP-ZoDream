15f394ad021bc72e49c5086771f6693e5fde69e1
zodream <648383079@qq.com>
2019-05-21 18:53:29 +0800

    aa

--- Module/CMS/Domain/ThemeManager.php
+++ Module/CMS/Domain/ThemeManager.php
@@ -3,11 +3,14 @@ namespace Module\CMS\Domain;
 
 use Module\CMS\Domain\Model\CategoryModel;
 use Module\CMS\Domain\Model\GroupModel;
+use Module\CMS\Domain\Model\ModelFieldModel;
 use Module\CMS\Domain\Model\ModelModel;
+use Module\CMS\Module;
 use Module\Template\Domain\Model\Base\OptionModel;
 use Zodream\Disk\Directory;
 use Zodream\Helpers\Json;
 use Zodream\Helpers\Str;
+use Zodream\Infrastructure\Error\Exception;
 
 class ThemeManager {
 
@@ -48,12 +51,25 @@ class ThemeManager {
         }
     }
 
-    protected function getCacheId($name, $prefix) {
-        $key = sprintf('@%s:%s', $prefix, $name);
+    protected function getCacheId($name, $prefix = null) {
+        $key = empty($prefix) && substr($name, 0, 1) === '@' ? $name : sprintf('@%s:%s', $prefix, $name);
         return isset($this->cache[$key]) ? $this->cache[$key] : 0;
     }
 
     protected function runScript($data) {
+        usort($data, function ($pre, $next) {
+            $maps = ['group' => 1, 'model' => 2, 'field' => 3, 'channel' => 4, 'content' => 5];
+            if (!isset($maps[$pre['action']])) {
+                return -1;
+            }
+            if (!isset($maps[$next['action']])) {
+                return 1;
+            }
+            if ($maps[$pre['action']] > $maps[$next['action']]) {
+                return 1;
+            }
+            return $maps[$pre['action']] < $maps[$next['action']] ? -1 : 0;
+        });
         foreach ($data as $item) {
             $method = 'runAction'.Str::studly($item['action']);
             if (method_exists($this, $method)) {
@@ -75,19 +91,87 @@ class ThemeManager {
     }
 
     protected function runActionModel($data) {
-
+        $fields = isset($data['fields']) ? $data['fields'] : [];
+        unset($data['fields'], $data['action']);
+        if (isset($data['setting']) && is_array($data['setting'])) {
+            $data['setting'] = Json::encode($data['setting']);
+        }
+        $model = ModelModel::create($data);
+        if (!$model) {
+            throw new Exception('数据错误');
+        }
+        $this->setCache([$model->table => $model->id, $model->id => $model], 'model');
+        Module::scene()->setModel($model)->initTable();
+        foreach ($fields as $field) {
+            $field['model_id'] = $model->id;
+            $this->runActionField($field);
+        }
     }
 
     protected function runActionField($data) {
-
+        if (isset($data['model'])) {
+            $data['model_id'] = $this->getCacheId($data['model']);
+        }
+        if (isset($data['action']) && $data['action'] === 'disable') {
+            ModelFieldModel::query()->where('model_id', $data['model_id'])
+                ->where('field', $data['field'])
+                ->updateBool('is_disable');
+            return;
+        }
+        unset($data['model'], $data['action']);
+        if (isset($data['setting']) && is_array($data['setting'])) {
+            $data['setting'] = Json::encode($data['setting']);
+        }
+        $model = ModelFieldModel::create($data);
+        if (!$model) {
+            throw new Exception('数据错误');
+        }
+        $scene = Module::scene()->setModel($this->getCacheId($model->model_id, 'model'));
+        $scene->addField($model);
     }
 
     protected function runActionChannel($data) {
+        if (!isset($data['type'])) {
 
+        } elseif ($data['type'] === 'page') {
+            $data['type'] = CategoryModel::TYPE_PAGE;
+        } elseif ($data['type'] === 'link') {
+            $data['type'] = CategoryModel::TYPE_LINK;
+        } else {
+            $data['type'] = CategoryModel::TYPE_CONTENT;
+            $data['model_id'] = $this->getCacheId($data['model']);
+        }
+        $children = isset($data['children']) ? $data['children'] : [];
+        if (isset($data['setting']) && is_array($data['setting'])) {
+            $data['setting'] = Json::encode($data['setting']);
+        }
+        $model = CategoryModel::create($data);
+        if (!$model) {
+            throw new Exception('数据错误');
+        }
+        $this->setCache([$model->name => $model->id, $model->id => $model], 'channel');
+        foreach ($children as $item) {
+            if (isset($item['action']) && $item['action'] === 'content') {
+                $item['cat_id'] = $model->id;
+                $this->runActionContent($item);
+                continue;
+            }
+            $item['parent_id'] = $model->id;
+            $this->runActionChannel($item);
+        }
     }
 
     protected function runActionContent($data) {
-
+        if (!is_numeric($data['cat_id'])) {
+            $data['cat_id'] = $this->getCacheId($data['cat_id']);
+        } elseif (isset($data['type'])) {
+            $data['cat_id'] = $this->getCacheId($data['type']);
+        }
+        unset($data['type'], $data['action']);
+        $cat = $this->getCacheId($data['cat_id'], 'channel');
+        $field_list = ModelFieldModel::where('model_id', $cat->model_id)->all();
+        $scene = Module::scene()->setModel($this->getCacheId($cat->model_id, 'model'));
+        $scene->insert($data, $field_list);
     }
 
     protected function insertGroup($item) {
