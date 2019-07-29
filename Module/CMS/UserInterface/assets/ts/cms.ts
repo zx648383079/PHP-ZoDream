86fd89c31b6d4a5cbee12206dc9ee42f6e94b6e6
ZoDream <648383079@qq.com>
2019-07-29 22:26:52 +0800

    修改CMS 网址导航

--- Module/CMS/UserInterface/assets/ts/cms.ts
+++ Module/CMS/UserInterface/assets/ts/cms.ts
@@ -1,3 +1,17 @@
+function bindNavigation() {
+    $('.search-box').on('mouseover', '.search-icon', function() {
+        $(".search-engine").show();
+    });
+    $('.self-box').on('click', '.custom-btn', function() {
+        let box = $(this).closest('.self-box');
+        box.toggleClass('edit-mode');
+    }).on('click', '.panel-close', function() {
+        $(this).closest('.self-box').removeClass('edit-mode');
+    }).on('hover', '.site-item', function() {
+        let box = $(this).closest('.self-box');
+    });
+}
+
 $(function() {
     $(document).on('click', ".tab-box .tab-header .tab-item", function() {
         let $this = $(this);
