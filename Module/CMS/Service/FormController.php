7944166d5bdf870f7fa90ef64e24984e44a2f6db
zodream <648383079@qq.com>
2019-08-17 14:19:43 +0800

    修改

--- Module/CMS/Service/FormController.php
+++ Module/CMS/Service/FormController.php
@@ -16,6 +16,9 @@ class FormController extends Controller {
 
     public function saveAction($id) {
         $model = ModelModel::find($id);
+        if (empty($model) || $model->type < 1) {
+            return $this->jsonFailure('表单数据错误');
+        }
         $field_list = ModelFieldModel::where('model_id', $model->id)->all();
         $scene = Module::scene()->setModel($model);
         $data = app('request')->get();
