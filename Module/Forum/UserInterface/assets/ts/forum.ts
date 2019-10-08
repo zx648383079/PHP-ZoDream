d52b6bc1859be4c1ead375d98e29d9b6027d569b
ZoDream <648383079@qq.com>
2019-10-08 22:36:53 +0800

    修改bbs 编辑器

--- Module/Forum/UserInterface/assets/ts/forum.ts
+++ Module/Forum/UserInterface/assets/ts/forum.ts
@@ -16,7 +16,14 @@ class Editor {
     public textField: HTMLTextAreaElement;
 
     private init() {
-
+        this.element.prepend(`<div class="editor-plugin">
+        <i class="fa fa-code" title="插入代码"></i>
+        <i class="fa fa-eye-slash" title="插入隐藏内容""></i>
+        <i class="fa fa-download" title="插入可下载内容"></i>
+        <i class="fa fa-image" title="插入图片"></i>
+        <i class="fa fa-video" title="插入视频"></i>
+        <i class="fa fa-link" title="插入链接"></i>
+    </div>`);
     }
 
     private bindEvent() {
@@ -24,21 +31,26 @@ class Editor {
         this.element.on('keydown', 'textarea', function(e) {
             if (e.keyCode === 9) {
                 e.preventDefault();
-                that.insert('    ', 4, true);
+                that.saveRange();
+                that.insertTab();
             }
         }).on('blur', 'textarea', function(e) {
-            that.range = {
-                start: this.selectionStart,
-                end: this.selectionEnd
-            };
+            that.saveRange();
         }).on('click', '.editor-plugin .fa', function() {
             let plugin = $(this);
+            if (that.isInAttr()) {
+                return;
+            }
+            let tag = that.getBlock();
+            if (tag && tag !== 'hide' && (tag !== 'a' || !plugin.hasClass('fa-image'))) {
+                return;
+            }
             if (plugin.hasClass('fa-code')) {
                 that.insert('<code></code>', 6, true);
                 return;
             }
             if (plugin.hasClass('fa-eye-slash')) {
-                that.insert('<hide></hide>', 6, true);
+                that.append('<hide></hide>', 6, true);
                 return;
             }
             if (plugin.hasClass('fa-download')) {
@@ -72,9 +84,120 @@ class Editor {
         }
     }
 
+    /**
+     * name
+     */
+    public saveRange() {
+        this.range = {
+            start: this.textField.selectionStart,
+            end: this.textField.selectionEnd
+        }
+    }
+
+    /**
+     * isInAttr
+     */
+    public isInAttr() {
+        if (!this.range || this.range.start < 3) {
+            return false;
+        }
+        if (!/\<[^\<\>]+$/.test(this.textField.value.substr(0, this.range.start))) {
+            return false;
+        }
+        if (this.range.start < this.range.end && /[\<\>]/.test(this.textField.value.substr(this.range.start, this.range.end - this.range.start))) {
+            return false;
+        }
+        if (!/^[^\<\>]*\>/.test(this.textField.value.substr(this.range.end))) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * getBlock
+     */
+    public getBlock(): string {
+        if (!this.range) {
+            return null;
+        }
+        let val = this.textField.value;
+        if (val.length < 3) {
+            return null;
+        }
+        let x = this.range.start;
+        if (x < 2) {
+            return null;
+        }
+        let end = 0;
+        let status = 0;
+        while (x > 0) {
+            x --;
+            let code = val.charAt(x);
+            if (status === 0 && code === '>') {
+                status = 1;
+                continue;
+            }
+            if (status === 1 && code === '/') {
+                status = 2;
+                continue;
+            }
+            if (status === 2) {
+                if (code !== '<') {
+                    status = 1;
+                    continue;
+                }
+                status = 0;
+                end ++;
+                continue;
+            }
+            if (status === 1 && code == '<') {
+                if (end > 0) {
+                    end --;
+                    continue;
+                }
+                return this.getBlockTag(x + 1, val);
+            }
+        }
+        return null;
+    }
+
+    private getBlockTag(x: number, val: string) {
+        let tag = [];
+        while (x < val.length) {
+            let code = val.charAt(x);
+            if (code === ' ' || code === '>') {
+                break;
+            }
+            tag.push(code);
+            x ++;
+        }
+        return tag.join('');
+    }
+
+    /**
+     * insertTab
+     */
+    public insertTab() {
+        if (!this.isInAttr()) {
+            return this.insert('    ', 4, true);
+        }
+        let x = this.range.start;
+        let val = this.textField.value;
+        while (true) {
+            let code = val.charAt(x);
+            if (code === '>') {
+                break;
+            }
+            x ++;
+        }
+        this.range.start = x + 1;
+        this.range.end = Math.max(x + 1, this.range.end)
+        this.focus();
+    }
+
     public insert(val: string, move: number = 0, focus: boolean = true) {
         this.checkRange();
-        this.textField.value = this.textField.value.substr(0, this.range.start)+ val + this.textField.value.substr(this.range.start);
+        this.textField.value = this.textField.value.substr(0, this.range.start) + val + this.textField.value.substr(this.range.start);
         this.move(move);
         if (!focus) {
             return;
@@ -107,7 +230,7 @@ class Editor {
             if (move < 1) {
                 return str + val;
             }
-            if (move > str.length) {
+            if (move > val.length) {
                 return val + str;
             }
             return val.substr(0, move) + str + val.substr(move);
@@ -137,7 +260,6 @@ class Editor {
         this.textField.selectionEnd = this.range.end;
         this.textField.focus();
     }
-
 
 
 }
