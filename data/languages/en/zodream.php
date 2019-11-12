ab89372b54e604f9da95593913c46663dda7a585
zodream <648383079@qq.com>
2019-11-12 17:16:55 +0800

    修改

--- data/languages/en/zodream.php
+++ data/languages/en/zodream.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'site keywords' => 'zodream,blog,coder blog,ts,vue,php,uwp,angular',
-    'site description' => 'from one,to one,simple blog',
-    'site title' => 'simple blog',
+    'site keywords' => 'zodream,diary,coder diary,ts,vue,php,uwp,angular',
+    'site description' => 'from one,to one,simple diary',
+    'site title' => 'simple diary',
     'loading...' => 'LOADING...',
     'back home' => 'Back Home',
     'error page' => ':( Sorry, Not Found Page!',
     'home' => 'Home',
-    'blog' => 'Blog',
+    'blog' => 'Diary',
     'friend link' => 'Friend Link',
     'about' => 'About',
     'cookie tip' => 'By using our site, you acknowledge that you have read and understand our Cookie Policy, Privacy Policy, and our Terms of Service.',
