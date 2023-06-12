1783bde737e02efc5bf4098cda5c3d2e874cc8ca
ZoDream <648383079@qq.com>
2023-06-12 12:04:49 +0800

    Update RouteTest.php

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -3,20 +3,22 @@ namespace Tests;
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Zodream\Route\ModuleRoute;
+use Zodream\Infrastructure\Support\RouteHelper;
 
 final class RouteTest extends TestCase {
 
     public static function pathProvider(): array
     {
         return [
-            ['blog', 'blog/aaa'],
+            ['blog', 'blog/aaa', 'index.php', 'blog/aaa'],
+            ['blog', '/blog/aaa', 'api/index.php', 'api/blog/aaa'],
+            ['blog', '/blog/aaa', 'api/install.php', 'api/install.php/blog/aaa'],
         ];
     }
 
     #[DataProvider('pathProvider')]
-    public function testMatch(string $module, string $path) {
-        $router = new ModuleRoute();
-        $this->assertTrue($router->isMatch($path, $module));
+    public function testMatch(string $module, string $pathInfo, string $script, $path) {
+        $this->assertTrue(RouteHelper::startWithRoute($pathInfo, $module));
+        $this->assertEquals(RouteHelper::getPathInfo($path, $script), $pathInfo);
     }
 }
